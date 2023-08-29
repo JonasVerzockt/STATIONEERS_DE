469408c0488a02b8c6a2f7dd657750ce9f3be7f5
JonasVerzockt <85904838+JonasVerzockt@users.noreply.github.com>
2023-08-29 05:39:06 +0200

    Update translator.py

--- PY/translator.py
+++ PY/translator.py
@@ -3,50 +3,77 @@ import xml.etree.ElementTree as ET
 import re
 
 # Setze deinen DeepL API-Schlüssel hier ein
-DEEPL_API_KEY = "TOKEN"
+DEEPL_API_KEY = ""
 
 # URL für die DeepL API
 DEEPL_API_URL = "https://api-free.deepl.com/v2/translate"
 
 # Funktion, um Text von Englisch nach Deutsch zu übersetzen
 def translate_text(text):
+    if text is None:
+        return None
+    
     # Extrahiere den Text innerhalb der geschweiften Klammern
     placeholders = re.findall(r'\{(.*?)\}', text)
-    modified_text = text
-    for idx, placeholder in enumerate(placeholders):
-        modified_text = modified_text.replace("{" + placeholder + "}", f"{{{idx}}}")
     
-    params = {
-        "auth_key": DEEPL_API_KEY,
-        "text": modified_text,
-        "target_lang": "DE"
-    }
-    response = requests.get(DEEPL_API_URL, params=params)
-    translation = response.json()["translations"][0]["text"]
-    
-    # Setze die Platzhalter an ihren ursprünglichen Platz
-    for idx, placeholder in enumerate(placeholders):
-        translation = translation.replace(f"{{{idx}}}", "{" + placeholder + "}")
+    if placeholders:
+        modified_text = text
+        for idx, placeholder in enumerate(placeholders):
+            modified_text = modified_text.replace("{" + placeholder + "}", f"{{{idx}}}")
+        
+        params = {
+            "auth_key": DEEPL_API_KEY,
+            "text": modified_text,
+            "target_lang": "DE"
+        }
+        response = requests.get(DEEPL_API_URL, params=params)
+        translation = response.json()["translations"][0]["text"]
+        
+        # Setze die Platzhalter an ihren ursprünglichen Platz
+        for idx, placeholder in enumerate(placeholders):
+            translation = translation.replace(f"{{{idx}}}", "{" + placeholder + "}")
+    else:
+        params = {
+            "auth_key": DEEPL_API_KEY,
+            "text": text,
+            "target_lang": "DE"
+        }
+        response = requests.get(DEEPL_API_URL, params=params)
+        translation = response.json()["translations"][0]["text"]
     
     return translation
 
-# Pfade zur XML-Datei
+# Pfad zur XML-Datei
 xml_file_path = "english.xml"
 
 # XML-Datei einlesen
 tree = ET.parse(xml_file_path)
 root = tree.getroot()
 
-# Schleife über alle Elemente mit den Tags "Value" und "Description"
-for element in root.iter("Value"):
-    original_text = element.text
-    translated_text = translate_text(original_text)
-    element.text = translated_text
+total_elements = 0
+for element in root:
+    total_elements += len(list(element.iter("Value")))
+    total_elements += len(list(element.iter("Description")))
 
-for element in root.iter("Description"):
-    original_text = element.text
-    translated_text = translate_text(original_text)
-    element.text = translated_text
+translated_elements = 0
+
+# Schleife über alle Wurzelelemente
+for root_element in root:
+    for idx, element in enumerate(root_element.iter("Value")):
+        original_text = element.text
+        translated_text = translate_text(original_text)
+        element.text = translated_text
+        translated_elements += 1
+        progress = (translated_elements / total_elements) * 100
+        print(f"Übersetze Element Value {translated_elements}/{total_elements} ({progress:.2f}% abgeschlossen)")
+
+    for idx, element in enumerate(root_element.iter("Description")):
+        original_text = element.text
+        translated_text = translate_text(original_text)
+        element.text = translated_text
+        translated_elements += 1
+        progress = (translated_elements / total_elements) * 100
+        print(f"Übersetze Element Description {translated_elements}/{total_elements} ({progress:.2f}% abgeschlossen)")
 
 # Übersetzte XML-Datei speichern
 translated_xml_file_path = "german.xml"
